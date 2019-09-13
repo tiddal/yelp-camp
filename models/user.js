fff477863bdb6425202f4a677054c2203125ff6a
tiddal <tiddal@sapo.pt>
2019-09-13 09:04:53 +0100

    update user model

--- models/user.js
+++ models/user.js
@@ -5,7 +5,12 @@ const mongoose = require('mongoose'),
 //	Schemas
 const userSchema = new mongoose.Schema({
 	username: String,
-	password: String
+	password: String,
+	firstName: String,
+	lastName: String,
+	email: String,
+	avatar: String,
+	isAdmin: { type: Boolean, default: false }
 });
 userSchema.plugin(passportLocalMongoose);
 const User = mongoose.model('User', userSchema);
